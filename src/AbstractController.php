dcceb6cd55f6462523178b5acaceddd8bfde7253
IgorDePaula <principe.borodin@gmail.com>
2015-04-13 11:39:45 -0300

    Removed unnused statements

--- src/AbstractController.php
+++ src/AbstractController.php
@@ -5,31 +5,29 @@ namespace Mvc;
 use Inspector\Controller;
 use Inspector\Controller\Route;
 
-abstract class AbstractController implements MvcInterface
-{
-	protected $params = array();
+abstract class AbstractController implements MvcInterface {
 
-	public function getInspector(MvcInterface $controller)
-	{
-		$inspectorController = new Controller();
-		$inspectorController->setObject($controller);
-		return $inspectorController;
-	}
+    protected $params = array();
 
-	public function validate(Route $route, $method){
-		if($route->getRoute()) 
-		{			
-			$this->setParam($route->getParams());
-			$this->{$method}();
-		}		
-	}
+    public function getInspector(MvcInterface $controller) {
+        $inspectorController = new Controller();
+        $inspectorController->setObject($controller);
+        return $inspectorController;
+    }
 
-	public function setParam(array $data)
-	{
-		$this->params = $data;
-	}
+    public function validate(Route $route, $method) {
+        if ($route->getRoute()) {
+            $this->setParam($route->getParams());
+            $this->{$method}();
+        }
+    }
 
-	public function getParams(){
-		return $this->params;
-	}
+    public function setParam(array $data) {
+        $this->params = $data;
+    }
+
+    public function getParams() {
+        return $this->params;
+    }
+
 }
